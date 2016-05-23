9d548b3ec305b7844bae57a0e3a5caa8ac15a13c
Ivan Kyselov <kyselovivan@gmail.com>
2016-05-23 13:30:12 +0300

    menu_main & values-21 added

--- app/src/main/java/ua/kyselov/android/bitsandpizzas/MainActivity.java
+++ app/src/main/java/ua/kyselov/android/bitsandpizzas/MainActivity.java
@@ -1,13 +1,34 @@
 package ua.kyselov.android.bitsandpizzas;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_create_order:
+                return true;
+            case R.id.action_share:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
