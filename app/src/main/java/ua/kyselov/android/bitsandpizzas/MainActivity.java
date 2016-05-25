fbd672e36c1e58bdbb0d5c4d84539c9cf1a65545
Ivan Kyselov <kyselovivan@gmail.com>
2016-05-25 16:34:57 +0300

    DrawerItemClickListener.class added

--- app/src/main/java/ua/kyselov/android/bitsandpizzas/MainActivity.java
+++ app/src/main/java/ua/kyselov/android/bitsandpizzas/MainActivity.java
@@ -1,22 +1,39 @@
 package ua.kyselov.android.bitsandpizzas;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ShareActionProvider;
 
 
 public class MainActivity extends Activity {
 
     private ShareActionProvider shareActionProvider;
+    private String[] titles;
+    private ListView drawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView)findViewById(R.id.drawer);
+        drawerList.setAdapter(new ArrayAdapter<String>(
+                this,android.R.layout.simple_list_item_activated_1,titles)
+        );
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
     @Override
@@ -47,5 +64,47 @@ public class MainActivity extends Activity {
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_TEXT, text);
         shareActionProvider.setShareIntent(intent);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position){
+        Fragment fragment;
+        switch (position){
+            case 1:
+                fragment = new PizzaFragment();
+                break;
+            case 2:
+                fragment = new PastaFragment();
+                break;
+            case 3:
+                fragment = new StoresFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+                break;
+        }
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame,fragment);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+        setActionBarTitile(position);
+    }
+
+    private void setActionBarTitile(int position){
+        String title;
+        if(position == 0){
+            title = getResources().getString(R.string.app_name);
+        }
+        else{
+            title = titles[position];
+        }
+        getActionBar().setTitle(title);
     }
 }
